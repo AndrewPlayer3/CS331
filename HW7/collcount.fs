1258151fa74fefb22980fd6b32a502aa8541832e
Andrew Player <andrewplayer3@gmail.com>
2022-04-26 10:04:37 -0800

    cleaned forth program

--- HW7/collcount.fs
+++ HW7/collcount.fs
@@ -1,43 +1,28 @@
 ( File:   collcount.fs                )
 ( Author: Andrew Player, 2022         )
-(                                     )
+( ----------------------------------- )
 ( Counts the stopping time for the    )
 ( Collatz Conjecture for a given num. )
 
-
-( Words for Odd Case )
-
-: ifoddadd 1 + ;
-: ifodddiv 3 * ;
-: ifodd 
-    ifodddiv 
-    ifoddadd ;
-
-
-( Word for Even Case )
-
-: ifeven 2 / ;
-
-
 ( Collatz Operation )
-
-: coll dup 
+( ----------------- )
+: coll 
+    dup 
     1 = 
         if
         else 
             dup 
             2 mod 0 = 
                 if  
-                    ifeven 
+                    2 / 
                 else 
-                    ifodd 
+                    3 * 1 + 
                 then 
                     swap 1 + swap 
         then ;
 
-
 ( Collatz Loop )
-
+( ------------ )
 : collcount 
     0 swap 
     begin 
@@ -46,4 +31,4 @@
         while 
             coll 
         repeat 
-            drop . ;
+            drop ;
